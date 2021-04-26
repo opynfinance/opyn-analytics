791d8efa6129a70f33c82015da086187797e62b8
ugolino <dan.ugolini@gmail.com>
2021-04-26 14:05:15 +0200

    navbar design

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="columns is-mobile is-centered mt-4">
-   
-      <figure class="image is-inline-block" >
-        <img :src="require('../assets/logo.svg')" width="200px">
-      </figure>
-
+    <figure class="image is-inline-block" >
+      <img :src="require('../assets/logo.svg')" width="200px">
+    </figure>
   </div>
 </template>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -3,26 +3,22 @@
     <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-menu">
         <div class="navbar-end">
-
-    
-            <router-link 
-              :class="`navbar-item ${this.$route.name === 'Volumes' ? 'is-active' : ''}`" 
-              to="/" >
-              Volumes
-            </router-link>
-   
-            <router-link 
-              :class="`navbar-item ${this.$route.name === 'Addresses' ? 'is-active' : ''}`" 
-              to="/addresses">
-              Addresses
-            </router-link>
-
+          <router-link 
+            :class="`navbar-item ${this.$route.name === 'Volumes' ? 'is-active' : ''}`" 
+            to="/" >
+            Volumes
+          </router-link>
+  
+          <router-link 
+            :class="`navbar-item ${this.$route.name === 'Addresses' ? 'is-active' : ''}`" 
+            to="/addresses">
+            Addresses
+          </router-link>
         </div>
       </div>
     </nav>
 
-     <Logo />
-
+    <Logo />
     <h1 class="subtitle has-text-centered">Opyn v2 Gamma Protocol Analytics</h1>
 
   </div>
